fix scaling, bit depth

app.py
@@ -75,10 +75,11 @@ def capture():
             for r in res:
                   if r.shape[2]==1:
                         #r = ircapture()
-                        cv2.imwrite(os.path.join(web,'bar.bmp'),r)
                         fname = current_time+'_'+current_spot+'_ir.png'
                         logging.info(fname)
                         cv2.imwrite(os.path.join(p,fname),r)
+                        np.right_shift(r, 8, r)
+                        cv2.imwrite(os.path.join(web,'bar.bmp'),np.uint8(r))
                   else:
                         #r = bgrcapture(ry,rx)
                         cv2.imwrite(os.path.join(web,'foo.bmp'),r)

logger.py
@@ -72,7 +72,8 @@ def irpcapture():
             fname = current_time+'_'+current_spot+'_ir.png'
             logging.info(os.path.join(p,fname))
             cv2.imwrite(os.path.join(p,fname),r)
-            cv2.imwrite(os.path.join(web,'bar.bmp'),r)
+            np.right_shift(r, 8, r)
+            cv2.imwrite(os.path.join(web,'bar.bmp'),np.uint8(r))
             sleep(app.config['waittime'])
 def smap(f):
       return f()

process.py
@@ -32,7 +32,7 @@ for f in files:
         lat = np.nan
         
     if 'bgr' in f:
-        bgr = cv2.imread(os.path.join(p,f))
+        bgr = cv2.imread(os.path.join(p,f),cv2.IMREAD_UNCHANGED)
         hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
         h,s,v = cv2.split(hsv)
         img_size = h.shape
@@ -45,6 +45,6 @@ for f in files:
         cv2.imwrite(os.path.join(p2,fseg),classes)
         logging.info(os.path.join(p2,fseg))
     if 'ir' in f:
-        ir = cv2.imread(os.path.join(p,f))
+        ir = cv2.imread(os.path.join(p,f),cv2.IMREAD_UNCHANGED))
 
         