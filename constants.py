b31f6815423dd3eccdb79624a2fe1ae3453395b0
Joaquin Casanova <joaquin.casanova@ceres-login.scinet.local>
2022-06-23 14:19:30 -0500

    parameter assignment

--- constants.py
+++ constants.py
@@ -1,5 +1,12 @@
 from scipy import constants
+import os
+from pandas import read_csv, DataFrame
+import logging
+logging.basicConfig(level=logging.INFO)
+home = os.path.join("/home","pi")
+p = os.path.join(home,'CookIRCamET','Inputs')
 
+#physical constants
 pi = constants.pi
 g = constants.g #m/s2
 boltz = constants.Stefan_Boltzmann# W/K4/m2
@@ -15,36 +22,41 @@ c_pv = 1865
 # heat capacity of mixed air at constant pressure (J kg-1 K-1)
 c_p = 1013
 
-Cx = 90#Empirical constant  s m-1
-
-b = 0.012#Empirical constant = 0.012 or b = 0.012[1 + cos(psiw)],
-#       where psiw is wind direction, ccw from north
-c = 0.0025#Empirical constant = 0.0025
 # ratio of the molecular weight of water vapor to dry air
 epsilon = 0.622
 # gas constant for dry air, J/(kg*degK)
 R_d = 287.04
 
+class inputs:
+    def __init__(self):
+        self.read_params()
+        self.read_indata()
+        return None
+
+    def read_params(self):
+        for f in os.listdir(p):
+            if 'parameters' in f and f[-4:]=='.csv':
+                parameters = read_csv(os.path.join(p,f))
+#defaults
+#user adjustable parameters
+Cx = 90#Empirical constant  s m-1
+b = 0.012#Empirical constant = 0.012 or b = 0.012[1 + cos(psiw)],
+#       where psiw is wind direction, ccw from north
+c = 0.0025#Empirical constant = 0.0025        
 #Monin-Obukhov parameters
 dhc = .67#displacement height/canopy height C&N98, K95 say 0.65
 zomhc = 0.125#momemtun roughness over canopy height K95
 zohzom = 1#heat roughness over momentum roughness C&N say .2,
 # K95 say exp(-2),
 # K99 set equal for series model
-
-#Measurement heights (speed and temp)
-zu = 2
-zt = 2
-
 #bare soil roughness
-#hs = .4e-3 #m
-#zombs = 1 #m
-#zohzom = np.exp(-4.5) #from Stewart paper
+hs = .4e-3 #m
+zombs = 1 #m
+zohzom = np.exp(-4.5) #from Stewart paper
 #Stewart, J. B., W. P. Kustas, K. S. Humes, W. D. Nichols, M. S.
 #Moran, and H. A. R. de Bruin, 1994: Sensible heat fluxradiometric
 #surface temperature relationship for eight semiarid
 #areas. J. Appl. Meteor., 33, 1110–1117.
-
 #soil heat flux
 GRnDay = -0.1
 GRnNight = -0.5
@@ -52,32 +64,24 @@ aG = -0.31
 #canopy stomatal resistance s/m
 rcDay = 50
 rcNight = 200
-
 #soil albedo vis/nir/wet/dry
 rhosNir = 0.15
 rhosNis = 0.25
-
 #soil emissivity
 emisSoil = 0.98
-
 #residue albedo
-#rhorNir = 
-#rhorVis =
-
+rhorNir = 0.15 
+rhorVis = 0.25
 #residue emissivity
 emisRes = 0.98
-
 #leaf absorbance/emittance/reflectance
 emisVeg = 0.98
 zetaVis = 0.85#Leaf shortwave absorption in the visible (PAR) spectra (no units)
 zetaNir = 0.2#Leaf shortwave absorption in the near infrared spectra (no units)
-
 #leaf angle param
 XE = 1.0
-
 #canopy lw extinction
 kappaIR = 0.95
-
 #solar params
 PISI = 0.457#Fraction of visible shortwave irriadiance in global irradiance;
     #~0.457 at Bushland, which agrees with Meek et al. (1984) for other Western US locations
@@ -89,4 +93,21 @@ KbNirConst = 1.086#Constant used in empirical equation to calculate the fraction
              # of direct beam irradiance in the near-infrared (NIR) band (no units)
 KbNirExp = 2.384#Exponent used in empirical equation to calculate the fraction
            #of direct beam irradiance in the near-infrared (NIR) band (no units)
-                
+#don't consider residue a separate class
+modeltype = '2SEB'
+#do consider residue a separate class
+#modeltype = '3SEB'
+lat = np.nan#(dec deg)
+lon = np.nan#(dec deg)
+ele = np.nan#elevation (m)
+#Measurement heights (speed and temp)
+zu = 2
+zt = 2
+rowwidth = .6#row width (m)
+rowdir = # degrees from N, clockwise
+Lz = -120#longitud
+
+    def read_indata(self):
+        for f in os.listdir(p):
+            if 'timeseries' in f and f[-4:]=='.csv':
+                timeseries = read_csv(os.path.join(p,f))
--- tseb.py
+++ tseb.py
@@ -6,54 +6,23 @@ from datetime import datetime
 import os
 import numpy as np
 import utils, aero, canopy, fluxes, solar
+
 from pandas import read_csv, DataFrame
+from constants import *
 
 import logging
 logging.basicConfig(level=logging.INFO)
 home = os.path.join("/home","pi")
 p = os.path.join(home,'CookIRCamET','Working')
 
-#read from config file
+#read from config file and input file
+data = inputs()
+data.timereries
+data.parameters
 
-#measurements:
-modeltype = '2SEB'
-#1SEB
-#get Trad from mean of IR image
-#2SEB
-#get Tc, Ts
-#3SEB
-#get Tc, Ts, Tr
-#4SEB
-#get Tc,sh, Ts,sh, Tc,sn, Ts,sn
-
-lat = #(dec deg)
-lon = #(dec deg)
-ele = #elevation (m)
-
-zu = #measurement height of wind
-zt = #measurement height of temperature
-
-row = #row width
-
-#read from file
-#Ta, P, ea/RH
-#Tc, Ts, Tr, Trad
-
-#Rn/Rd
-#use Rn as input,Sd and Ld, or calculate solar from equation
-#RSopt 1, 2, 3
-
-#time
-#canopy height, fractions, width, LAI
-#if width is NA use width = row*fcanopy
-
-if modeltype=='1SEB':
-    #out_df = DataFrame(columns=['datetime (UTC)','sun elevation (deg)','sun azimuth (deg)','LE (W/m2)','H (W/m2)','G (W/m2)','Rn (W/m2)','ET (mm)'])
-elif modeltype=='2SEB':
+if  modeltype=='2SEB':
     #out_df = DataFrame(columns=['datetime (UTC)','sun elevation (deg)','sun azimuth (deg)','LEc (W/m2)','LEs (W/m2)','Hc (W/m2)','Hs (W/m2)','H (W/m2)','G (W/m2)','Sns (W/m2)','Snc (W/m2)','Lns (W/m2)','Lnc (W/m2)','E (mm)', 'T (mm)'])
 elif modeltype=='3SEB':
-    #out_df = DataFrame(columns=['datetime (UTC)','sun elevation (deg)','sun azimuth (deg)','LEc (W/m2)','LEs (W/m2)','LEr (W/m2)','Hc (W/m2)','Hs (W/m2)','Hr (W/m2)','G (W/m2)','Sns (W/m2)','Snr (W/m2)','Snc (W/m2)','Lns (W/m2)','Lnr (W/m2)','Lnc (W/m2)','E (mm)', 'T (mm)'])
-elif modeltype=='4SEB':
     #out_df = DataFrame(columns=['datetime (UTC)','sun elevation (deg)','sun azimuth (deg)','LEc (W/m2)','LEs (W/m2)','LEr (W/m2)','Hc (W/m2)','Hs (W/m2)','Hr (W/m2)','G (W/m2)','Sns (W/m2)','Snr (W/m2)','Snc (W/m2)','Lns (W/m2)','Lnr (W/m2)','Lnc (W/m2)','E (mm)', 'T (mm)'])
 else:
     raise(NameError('Invalid model'))
