172026311c90d65fc8b0b07332081763c74c6ac9
joaquin.casanova <joaquin.casanova@gmail.com>
2023-08-02 15:25:09 -0700

    no flask

--- logger_uvc_af.py
+++ logger_uvc_af.py
@@ -1,31 +1,11 @@
 #!/usr/bin/env python3
 import sys
-import numpy as np
-import cv2
 from utils2 import *
 from time import sleep
-from datetime import datetime, timezone
 import os
-import numpy as np
-from flask import Flask, render_template, send_file, make_response, url_for, Response, redirect, request
-from multiprocessing import Pool, Process
 
 import logging
-
 logging.basicConfig(level=logging.DEBUG)
-
-#initialise app
-#app = Flask(__name__)
-#app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-#
-#@app.route('/' )
-#def index():
-      
-#      now = datetime.now()
-#      current_time = now.strftime("%Y%d%m%H%M%S")
-
-#      return render_template('camera.html', time=current_time)
 
 if __name__ == '__main__':     
       if len(sys.argv)>1:
@@ -42,15 +22,6 @@ if __name__ == '__main__':
                   sleep(waittime)
       except:
             del hq, flir
-
       
-      #app.config['waittime'] = waittime
-      #q1=Process(target=hq.capture)
-      #q2=Process(target=flir.capture)
-      #q1.start()
-      #q2.start()
-      #app.run(debug=False,host='0.0.0.0')
-      #q1.join()
-      #q2.join()
-
+      
       
