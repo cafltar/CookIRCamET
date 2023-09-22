45e8c7995f6f3e6e1cec09095e2390df95c7e022
joaquin.casanova <joaquin.casanova@gmail.com>
2023-09-22 16:36:03 -0700

    camera settings

--- logger_uvc_af.py
+++ logger_uvc_af.py
@@ -4,6 +4,7 @@ from utils2 import *
 from time import sleep
 import os
 
+from threading import Thread
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -11,7 +12,7 @@ if __name__ == '__main__':
       if len(sys.argv)>1:
             waittime = int(sys.argv[1])
       else:
-            waittime = 60
+            waittime = 3600
       flir = ir_cam(waittime)
       hq = bgr_cam(ry,rx,exp_time,frame_time,brightness,contrast,waittime)
 
@@ -20,7 +21,13 @@ if __name__ == '__main__':
                   r = hq.capture()
                   ir = flir.capture()
                   sleep(waittime)
+            #t1 = Thread(target = hq.run())
+            #t2 = Thread(target = flir.run())
+            #t1.start()
+            #t2.start()
       except:
+            #t1.join()
+            #t2.join()
             del hq, flir
       
       
