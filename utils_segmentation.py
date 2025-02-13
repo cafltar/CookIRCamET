6df4f7fc7d805dcbbcd355ca8162f81a2fafd1f1
joaquincasanova <joaquin.casanova@gmail.com>
2025-02-13 10:22:56 -0800

    paper stuf

--- utils_segmentation.py
+++ utils_segmentation.py
@@ -36,12 +36,14 @@ if platform.uname().system=='Windows':
     p1 = os.path.join(q,'Images','CookHY2024')
     p00 = os.path.join(q,'Images','CprlHY2023')
     p3 = os.path.join('F:','usda','work','CookIRCamET','Working')
+    p4 = os.path.join('F:','usda','work','CookIRCamET','Documents')
 else:
     q = os.path.join('../../','raw','CookIRCamET')
     p0 = os.path.join('../../','raw','CookIRCamET','Images','CookHY2023')
     p1 = os.path.join('../../','raw','CookIRCamET','Images','CookHY2024')
     p00 = os.path.join('../../','raw','CookIRCamET','Images','CprlHY2023')
     p3 = os.path.join('../../','work','CookIRCamET','Working')
+    p4 = os.path.join('../../','work','CookIRCamET','Documents')
 
 
 n_components = 8
